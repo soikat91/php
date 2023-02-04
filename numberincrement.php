073cd2834fc7a990e434a885ee03bd0243e9c9af
soikat <soikateman@gmail.com>
2023-02-04 17:07:08 +0600

    number incement logic

---
+++ numberincrement.php
@@ -0,0 +1,20 @@
+<?php
+$number=10;
+$result=$number++;
+/*etar logic hosse
+etar mane holo
+$result=$number ace tarpor
+$number=$number+1
+*/
+ echo $result ,"\n" ,$number;
+echo"\n";
+
+//  another one 
+
+$n=9;
+$m=++$n;
+/*Logic
+$n=$n+1;
+$n=$m;*/
+echo $m ,"\n",$n;
+
--- operatior.php
+++ operatior.php
@@ -4,5 +4,7 @@ $number=12;
 //$number+=1;//3 and 4 same things
 //$number++;//increment
 //$number--;
+//+=>operator
+//value=>operand
 $number =(3-1)/2;
 echo $number;
