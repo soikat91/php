789253e836f2249ce7ba948e6f364a5becf3f35b
soikat <soikateman@gmail.com>
2023-02-05 12:45:19 +0600

    number system added

---
+++ numbersystem.php
@@ -0,0 +1,14 @@
+<?php
+$decimal=16;
+$octal=012;
+$hexadecimal=0xAB;
+$binary=0101;
+printf("Number is %d , %d , %d,%d",$decimal,$octal,$hexadecimal,$binary);
+echo("\n");
+printf("Binary eqvalent : %d to %b",1212,1212);
+echo("\n");
+printf("Octal equvalent :%o to %d",012,012);
+echo("\n");
+printf("Decimal eqvalent :%x to %d",0x18A,0x189);
+echo("\n");
+printf("Hexadecimal equvalent :%d to %x",1212,1212);
